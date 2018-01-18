dcbb9c2a9d6408f61148f7280914b8da999c3fa2
Erik Tallang <e.tallang@gmail.com>
2018-01-18 17:23:23 +0100

    Initial commit.

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'td-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'td';
+}
