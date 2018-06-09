01da6a1dcdf3ea5b163602240609e86c7b3e122b
Erik Tallang <e.tallang@gmail.com>
2018-06-09 23:07:46 +0200

    chore: upgrade to Angular 6

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ThemeService } from '../core/services/theme.service';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'td-toolbar',
